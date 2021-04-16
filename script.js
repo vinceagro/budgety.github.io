ab664f0ec90a29dce67ee2f014aaa07ebc02c343
Vincenzo Agrillo <vincenzoagrillo@vincenzos-mbp.home>
2021-04-16 19:06:26 +0100

    final update, app completed

--- script.js
+++ script.js
@@ -1,52 +1,19 @@
 'use strict';
 
-// BANKIST APP
-
-// Data
-// const account1 = {
-//   owner: 'Jonas Schmedtmann',
-//   movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
-//   interestRate: 1.2, // %
-//   pin: 1111,
-// };
-
-// const account2 = {
-//   owner: 'Jessica Davis',
-//   movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
-//   interestRate: 1.5,
-//   pin: 2222,
-// };
-
-// const account3 = {
-//   owner: 'Steven Thomas Williams',
-//   movements: [200, -200, 340, -300, -20, 50, 400, -460],
-//   interestRate: 0.7,
-//   pin: 3333,
-// };
-
-// const account4 = {
-//   owner: 'Sarah Smith',
-//   movements: [430, 1000, 700, 50, 90],
-//   interestRate: 1,
-//   pin: 4444,
-// };
-
-// const accounts = [account1, account2, account3, account4];
-
 const account1 = {
-  owner: 'Jonas Schmedtmann',
-  movements: [200, 455.23, -306.5, 25000, -642.21, -133.9, 79.97, 1300],
+  owner: 'Vinnie Page',
+  movements: [100, 455.23, -306.5, 3200, -612.21, -123.9, 99.97, 300],
   interestRate: 1.2, // %
   pin: 1111,
 
   movementsDates: [
-    '2019-11-18T21:31:17.178Z',
-    '2019-12-23T07:42:02.383Z',
+    '2020-11-18T21:31:17.178Z',
+    '2020-12-23T07:42:02.383Z',
     '2020-01-28T09:15:04.904Z',
-    '2020-04-01T10:17:24.185Z',
-    '2020-05-08T14:11:59.604Z',
+    '2021-04-01T10:17:24.185Z',
+    '2021-05-08T14:11:59.604Z',
     '2020-05-27T17:01:17.194Z',
-    '2020-07-11T23:36:17.929Z',
+    '2021-07-11T23:36:17.929Z',
     '2020-07-12T10:51:36.790Z',
   ],
   currency: 'EUR',
@@ -54,20 +21,20 @@ const account1 = {
 };
 
 const account2 = {
-  owner: 'Jessica Davis',
-  movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
+  owner: 'Caroline Dorth',
+  movements: [400, 5400, -120, -990, -320, -1120, 7500, -30],
   interestRate: 1.5,
   pin: 2222,
 
   movementsDates: [
-    '2019-11-01T13:15:33.035Z',
-    '2019-11-30T09:48:16.867Z',
-    '2019-12-25T06:04:23.907Z',
-    '2020-01-25T14:18:46.235Z',
-    '2020-02-05T16:33:06.386Z',
-    '2020-04-10T14:43:26.374Z',
-    '2020-06-25T18:49:59.371Z',
-    '2020-07-26T12:01:20.894Z',
+    '2020-11-01T13:15:33.035Z',
+    '2019-11-30T09:48:46.867Z',
+    '2019-12-25T06:04:32.907Z',
+    '2021-01-25T14:18:46.235Z',
+    '2020-02-05T16:33:09.386Z',
+    '2020-04-10T14:33:16.374Z',
+    '2020-06-25T18:39:59.371Z',
+    '2020-07-26T12:21:20.894Z',
   ],
   currency: 'USD',
   locale: 'en-US',
@@ -135,7 +102,7 @@ const displayMovements = (acc, sort = false) => {
 
 const calcDisplayBalance = acc => {
   acc.balance = acc.movements.reduce((acc, el) => acc + el, 0);
-  labelBalance.textContent = `${acc.balance}€`;
+  labelBalance.textContent = `${acc.balance.toFixed(2)}€`;
 };
 
 const calcDisplaySummary = acc => {
@@ -146,7 +113,7 @@ const calcDisplaySummary = acc => {
   const outcomes = acc.movements
     .filter(el => el < 0)
     .reduce((acc, el) => acc + el);
-  labelSumOut.textContent = `${Math.abs(outcomes)}€`;
+  labelSumOut.textContent = `${Math.abs(outcomes.toFixed(2))}€`;
   const interests = acc.movements
     .filter(el => el > 0)
     .map(el => (el * acc.interestRate) / 100)
@@ -281,9 +248,3 @@ btnTransfer.addEventListener('click', transfer);
 btnClose.addEventListener('click', closeAccount);
 btnLoan.addEventListener('click', requestLoan);
 btnSort.addEventListener('click', sortMovements);
-
-const currencies = new Map([
-  ['USD', 'United States dollar'],
-  ['EUR', 'Euro'],
-  ['GBP', 'Pound sterling'],
-]);
