c69125ea5239cb8ec979fc447aa94e0e560f1a8b
Vincenzo Agrillo <vincenzoagrillo@vincenzos-mbp.home>
2021-04-16 16:38:59 +0100

    updated date

--- script.js
+++ script.js
@@ -3,11 +3,54 @@
 // BANKIST APP
 
 // Data
+// const account1 = {
+//   owner: 'Jonas Schmedtmann',
+//   movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
+//   interestRate: 1.2, // %
+//   pin: 1111,
+// };
+
+// const account2 = {
+//   owner: 'Jessica Davis',
+//   movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
+//   interestRate: 1.5,
+//   pin: 2222,
+// };
+
+// const account3 = {
+//   owner: 'Steven Thomas Williams',
+//   movements: [200, -200, 340, -300, -20, 50, 400, -460],
+//   interestRate: 0.7,
+//   pin: 3333,
+// };
+
+// const account4 = {
+//   owner: 'Sarah Smith',
+//   movements: [430, 1000, 700, 50, 90],
+//   interestRate: 1,
+//   pin: 4444,
+// };
+
+// const accounts = [account1, account2, account3, account4];
+
 const account1 = {
   owner: 'Jonas Schmedtmann',
-  movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
+  movements: [200, 455.23, -306.5, 25000, -642.21, -133.9, 79.97, 1300],
   interestRate: 1.2, // %
   pin: 1111,
+
+  movementsDates: [
+    '2019-11-18T21:31:17.178Z',
+    '2019-12-23T07:42:02.383Z',
+    '2020-01-28T09:15:04.904Z',
+    '2020-04-01T10:17:24.185Z',
+    '2020-05-08T14:11:59.604Z',
+    '2020-05-27T17:01:17.194Z',
+    '2020-07-11T23:36:17.929Z',
+    '2020-07-12T10:51:36.790Z',
+  ],
+  currency: 'EUR',
+  locale: 'pt-PT', // de-DE
 };
 
 const account2 = {
@@ -15,23 +58,22 @@ const account2 = {
   movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
   interestRate: 1.5,
   pin: 2222,
+
+  movementsDates: [
+    '2019-11-01T13:15:33.035Z',
+    '2019-11-30T09:48:16.867Z',
+    '2019-12-25T06:04:23.907Z',
+    '2020-01-25T14:18:46.235Z',
+    '2020-02-05T16:33:06.386Z',
+    '2020-04-10T14:43:26.374Z',
+    '2020-06-25T18:49:59.371Z',
+    '2020-07-26T12:01:20.894Z',
+  ],
+  currency: 'USD',
+  locale: 'en-US',
 };
 
-const account3 = {
-  owner: 'Steven Thomas Williams',
-  movements: [200, -200, 340, -300, -20, 50, 400, -460],
-  interestRate: 0.7,
-  pin: 3333,
-};
-
-const account4 = {
-  owner: 'Sarah Smith',
-  movements: [430, 1000, 700, 50, 90],
-  interestRate: 1,
-  pin: 4444,
-};
-
-const accounts = [account1, account2, account3, account4];
+const accounts = [account1, account2];
 
 // Elements
 const app = document.querySelector('.app');
@@ -60,16 +102,31 @@ const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
+const getCurrentTime = () => {
+  const now = new Date();
+  const day = `${now.getDate()}`.padStart(2, 0);
+  const year = now.getFullYear();
+  const month = `${now.getMonth() + 1}`.padStart(2, 0);
+  const hour = `${now.getHours()}`.padStart(2, 0);
+  const min = `${now.getMinutes()}`.padStart(2, 0);
+  labelDate.textContent = `${day}/${month}/${year}, ${hour}:${min}`;
+};
+
 const displayMovements = (acc, sort = false) => {
   containerMovements.innerHTML = '';
   const movs = sort
     ? acc.movements.slice().sort((a, b) => a - b)
     : acc.movements;
   movs.forEach((el, i, arr) => {
+    let date = new Date(acc.movementsDates[i]);
+    const day = `${date.getDate()}`.padStart(2, 0);
+    const year = date.getFullYear();
+    const month = `${date.getMonth() + 1}`.padStart(2, 0);
+    let displayDate = `${day}/${month}/${year}`;
     const type = el > 0 ? 'deposit' : 'withdrawal';
     const html = `<div class="movements__row">
     <div class="movements__type movements__type--${type}">${i + 1} ${type}</div>
-    <div class="movements__date"></div>
+    <div class="movements__date">${displayDate}</div>
     <div class="movements__value">${el}€</div>
   </div>`;
     containerMovements.insertAdjacentHTML('afterbegin', html);
@@ -108,10 +165,8 @@ const createUsername = accs => {
   });
 };
 createUsername(accounts);
-
-let currentAccount;
-
 const updateUI = acc => {
+  getCurrentTime();
   //Display movements
   displayMovements(acc);
   //Display balance
@@ -120,12 +175,21 @@ const updateUI = acc => {
   calcDisplaySummary(acc);
 };
 
+let currentAccount;
+
+//ALWAYS LOGGED
+
+currentAccount = account1;
+updateUI(currentAccount);
+containerApp.style.opacity = 100;
+
 const login = e => {
   e.preventDefault();
+  getCurrentTime();
   currentAccount = accounts.find(
     acc => acc.username === inputLoginUsername.value.trim()
   );
-  if (currentAccount?.pin === Number(inputLoginPin.value)) {
+  if (currentAccount?.pin === +inputLoginPin.value) {
     //Clear imput filds
     inputLoginUsername.value = inputLoginPin.value = '';
     inputLoginPin.blur();
@@ -138,7 +202,7 @@ const login = e => {
 
 const transfer = e => {
   e.preventDefault();
-  const amount = Number(inputTransferAmount.value);
+  const amount = +inputTransferAmount.value;
   const receiverUser = accounts.find(
     acc => acc.username === inputTransferTo.value
   );
@@ -150,7 +214,9 @@ const transfer = e => {
     receiverUser?.username !== currentAccount.username
   ) {
     receiverUser.movements.push(amount);
+    receiverUser.movementsDates.push(new Date().toISOString());
     currentAccount.movements.push(-amount);
+    currentAccount.movementsDates.push(new Date().toISOString());
   }
 
   updateUI(currentAccount);
@@ -160,7 +226,7 @@ const closeAccount = e => {
   e.preventDefault();
   if (
     inputCloseUsername.value === currentAccount.username &&
-    Number(inputClosePin.value) === currentAccount.pin
+    +inputClosePin.value === currentAccount.pin
   ) {
     const index = accounts.findIndex(
       acc => acc.username === currentAccount.username
@@ -173,10 +239,14 @@ const closeAccount = e => {
 
 const requestLoan = e => {
   e.preventDefault();
-  const amount = Number(inputLoanAmount.value);
+  const amount = +inputLoanAmount.value;
+
   if (amount > 0 && currentAccount.movements.some(mov => mov >= amount * 0.1)) {
-    currentAccount.movements.push(amount);
-    updateUI(currentAccount);
+    setTimeout(() => {
+      currentAccount.movements.push(amount);
+      currentAccount.movementsDates.push(new Date().toISOString());
+      updateUI(currentAccount);
+    }, 2000);
   } else {
     console.log(`You can't request more than 10% of the higher transiction.`);
   }
@@ -201,3 +271,5 @@ const currencies = new Map([
   ['EUR', 'Euro'],
   ['GBP', 'Pound sterling'],
 ]);
+
+setTimeout(() => console.log('PIZZA TIME'), 2000);
